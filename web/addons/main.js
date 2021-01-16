9224dff63ab71cc53e0cf24ae263ef70635d1600
Amanda <amanda02bishop@gmail.com>
2021-01-16 16:54:35 -0500

    Update main.js

--- web/addons/main.js
+++ web/addons/main.js
@@ -27,7 +27,7 @@ function formatData(data) {
             user["tod"] = habits[0];
             user["focus"] = habits[1];
             user["style"] = habits[2];
-            user["wantedMembers"] = line[i+5];
+            user["wantedMembers"] = parseInt(line[i+5]);
             user["group"] = line[i+6].split(' ');
             if (user["group"].includes('')) {
                 user["group"] = [];
@@ -51,7 +51,7 @@ var person = {
     tod: 'Afternoon',
     focus: 'Relaxed',
     style: 'Visual',
-    wantedMembers: '2',
+    wantedMembers: 3,
     group: []
 };  
 
@@ -60,7 +60,7 @@ function findMatches(person) {
     for (var student=0; student<students.length; student++) {
         if (students[student]["term"] == person["term"]) {
             for (var course=0; course<person["courses"].length; course++) {
-                if (students[student]["courses"].includes(person["courses"][course]) && !possibleMatches.includes(students[student]) && students[student]["wantedMembers"] != 1) {
+                if (students[student]["courses"].includes(person["courses"][course]) && !possibleMatches.includes(students[student]) && students[student]["wantedMembers"] != 1 && person["wantedMembers"] > students[student]["group"].length + 1) {
                     possibleMatches.push(students[student]);
                 }
             }
@@ -77,8 +77,24 @@ function addToTeam(person,memberName) {
     person["wantedMembers"]--;
     for (var student=0; student<students.length; student++) {
         if (students[student]["name"] == memberName) {
+            if (students[student]["group"].length != 0) {
+                for (var member=0; member<students[student]["group"].length; member++) {
+                    person["group"].push(students[student]["group"][member]);
+                    person["wantedMembers"]--;
+                    for (var s=0; s<students.length; s++) {
+                        if (students[s]["name"] == students[student]["group"][member]) {
+                            students[s]["group"].push(person["name"]);
+                            students[s]["wantedMembers"]--;
+                        }
+                    }
+                }
+            }
             students[student]["group"].push(person["name"]);
             students[student]["wantedMembers"]--;
         }
     }
 }
+
+addToTeam(person,"Giselle");
+//console.log(person);
+//console.log(students)
